dc1fa46b41c09b29154bcd7d668de01f8f03051b
Ameuma773 <98154029+Ameuma773@users.noreply.github.com>
2022-06-22 18:44:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -50,17 +50,10 @@ GetJson();
 GetQuake();
 setInterval(GetEEW, 1000);
 
-setInterval(GetQuake, 30000);
+setInterval(GetQuake, GetQuakeInterval);
 
 async function GetQuake() {
     var date = new Date();
-
-    //const NowTime = date.getFullYear() + '' + ('0' + (date.getMonth() + 1)).slice(-2) + '' + ('0' + date.getDate()).slice(-2) + '' + ('0' + date.getHours()).slice(-2) + '' + ('0' + date.getMinutes()).slice(-2) + '' + ('0' + date.getSeconds()).slice(-2);
-    //const NowDay = date.getFullYear() + '' + ('0' + (date.getMonth() + 1)).slice(-2) + '' + ('0' + date.getDate()).slice(-2);
-
-    //const NowTimeString = date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + date.getDate()).slice(-2) + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
-    //const clock = document.getElementById("clock");
-    //clock.innerText = NowTimeString;
 
     const url = "https://api.p2pquake.net/v2/jma/quake?limit=50";
 
